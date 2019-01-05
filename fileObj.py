1776d23a76ed0d4b8b4edee800c63af3edd9a8e8
James Pinnock <james-pinnock@hotmail.com>
2019-01-05 22:31:34 +0100

    Can enter in the number of files to create

--- fileObj.py
+++ fileObj.py
@@ -22,10 +22,12 @@
 import os
 
 
-def multi_file():
+def multi_file(input):
+    num_of_f = input
+    print(input)
     x = 0
     z = 0
-    while x < 3:
+    while x < num_of_f:
         with open ( f'test_{z}.txt', 'w' ) as w:
             pass
         x = x + 1
@@ -41,7 +43,9 @@ def folderMaker():
         dir_name = f"MadeByPython/{directory_name}"
         os.makedirs ( dir_name )
         os.chdir(dir_name)
-        multi_file ()
+        multi_file (10)
+
+
 
     except FileExistsError:
         print ( "File exists" )
@@ -49,3 +53,5 @@ def folderMaker():
 
 
 folderMaker()
+
+
